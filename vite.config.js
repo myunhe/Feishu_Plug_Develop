db5a4bd9046226f10b1412272c65faa254ab4d44
ruipeng.meng <ruipeng.meng@futurus.co>
2025-12-17 16:52:37 +0800

    修改相对路径

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 设置基础路径为相对路径
   server: {
     host: '0.0.0.0',
     port: 3000,
